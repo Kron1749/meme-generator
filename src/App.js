0d8b407875840e5d665ef66eef18a66bb3add07d
Kron1749 <drakgr98@gmail.com>
2022-08-15 17:26:17 +0300

    Created Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import Header from "./components/Header";
+import "./style.css";
 export default function App() {
-  return <h1>Hello world!</h1>;
+  return <Header />;
 }
